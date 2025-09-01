7c88ddf6f05a044b33950ab2590ba7bffed53764
kaleido34 <pashaantsharmaa@gmail.com>
2025-09-01 13:53:05 +0530

    minor change

--- app/page.tsx
+++ app/page.tsx
@@ -528,7 +528,7 @@ export default function Home() {
               <a href="#" aria-label="Facebook" className="rounded-full bg-[#F3F0FF] p-2 hover:bg-[#E5E5EF] transition"><Facebook className="h-5 w-5 text-[#7B5EA7]" /></a>
               <a href="#" aria-label="Instagram" className="rounded-full bg-[#F3F0FF] p-2 hover:bg-[#E5E5EF] transition"><Instagram className="h-5 w-5 text-[#7B5EA7]" /></a>
               <a href="#" aria-label="LinkedIn" className="rounded-full bg-[#F3F0FF] p-2 hover:bg-[#E5E5EF] transition"><svg className="h-5 w-5 text-[#7B5EA7]" fill="currentColor" viewBox="0 0 24 24"><path d="M19 0h-14c-2.76 0-5 2.24-5 5v14c0 2.76 2.24 5 5 5h14c2.76 0 5-2.24 5-5v-14c0-2.76-2.24-5-5-5zm-11 19h-3v-9h3v9zm-1.5-10.28c-.97 0-1.75-.79-1.75-1.75s.78-1.75 1.75-1.75 1.75.79 1.75 1.75-.78 1.75-1.75 1.75zm15.5 10.28h-3v-4.5c0-1.08-.02-2.47-1.5-2.47-1.5 0-1.73 1.17-1.73 2.39v4.58h-3v-9h2.88v1.23h.04c.4-.75 1.38-1.54 2.85-1.54 3.05 0 3.61 2.01 3.61 4.62v4.69z"/></svg></a>
-              <a href="#" aria-label="Github" className="rounded-full bg-[#F3F0FF] p-2 hover:bg-[#E5E5EF] transition"><Github className="h-5 w-5 text-[#7B5EA7]" /></a>
+              <a href="https://github.com/kaleido34/N2N" aria-label="Github" className="rounded-full bg-[#F3F0FF] p-2 hover:bg-[#E5E5EF] transition"><Github className="h-5 w-5 text-[#7B5EA7]" /></a>
             </div>
           </div>
           {/* Center: Navigation columns */}
